dd67459ed383c11f64087a7c9398b6c1a3551d3d
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2019-06-30 11:33:50 -0400

    started refactoring toward using keras.utils.Sequence

--- ai.py
+++ ai.py
@@ -14,7 +14,9 @@ need to:
 look into:
 	ALWAYS TRAINING 150 TIMES PER MESSAGES IS PRETTY INEFFICIENT SINCE MOST MESSAGES END WAY BEFORE THE 150TH CHARACTER, HOW DO I FIX THAT WITHOUT IT FORGETTING HOW TO END MESSAGES?
 		seems pretty googlable although it might send you down a rabbithole for a completely different type of rnn or something
-	might want to dockerize so I can run it on better computers easily 
+	should turn into a pip env so I can run it on other machines easier (dockerizing is going a bit overboard) 
+	should have an ExportedModel object or something that you export at the end that you can import in tony spark that just spits out a message given 3 messages
+		would be useful so that you dont have to worry about char conversion and stuff when you want to use the model in tony
 '''
 
 import json
@@ -23,7 +25,7 @@ import numpy
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 from keras.callbacks import ModelCheckpoint
-from keras.utils import np_utils
+from keras.utils import Sequence
 import itertools
 
 #technique/some copy & pasting from https://machinelearningmastery.com/text-generation-lstm-recurrent-neural-networks-python-keras/
@@ -36,24 +38,47 @@ def get_messages():
 
 
 class CharConverter:
+	"to convert message characters to a number and back again"
+
 	def __init__(self, messages):
 		self.all_chars = {char for msg in messages for char in msg['content']}
 		num_chars = len(self.all_chars) #used to normalize index to be between 0 and 1
 		self.get_char = {index/num_chars: char for index, char in enumerate(self.all_chars, start = 1)}
 		self.get_char[0] = '' #used for padding
-		self.get_int = {char: index for index, char in self.get_char}
+		self.get_num = {char: num for num, char in self.get_char}
 
 
 MAX_MSG_LENGTH = 150
 
+class ProcessedMessage:
+	"A more useful representation of a message dict"
+
+	def __init__(self, message, char_conv):
+		self.content = numpy.zeros(MAX_MSG_LENGTH)
+		for index, char in enumerate(message['content']):
+			self.content[index] = char_conv.get_num[char]
+		self.timestamp = float(message['created_at'])
+		self.channel = message['channel']['id']
+		self.author = message['author']['id']
+
 def group_messages_by_channel(messages):
 	messages_by_channel = dict()
 	for msg in short_messages:
-		chan_id = msg['channel']['id']
+		chan_id = msg.channel
 		if chan_id not in messages_by_channel:
 			messages_by_channel[chan_id] = []
 		messages_by_channel[chan_id].append(msg)
 	return messages_by_channel
+
+def preprocess_messages(messages, char_conv):
+	"remove long messages, turn all message dicts into ProcessedMessages, group by channel, sort each channel by time posted"
+	short_messages = (m for m in messages if len(m['content']) < MAX_MSG_LENGTH) #dont want massive messages to limit dimensionality 
+	processed_messages = (ProcessedMessage(m, char_conv) for m in messages)
+	messages_by_channel = group_messages_by_channel(short_messages)
+    get_timestamp = lambda msg: float(msg['created_at'])
+	messages_by_channel = list(sorted(chan, key = get_timestamp) for chan in messages_by_channel) #sort each messages by time
+	return messages_by_channel
+
 
 def normalize_time_delta(delta):
 	'''
@@ -67,25 +92,22 @@ def normalize_time_delta(delta):
 		) + 1
 	) / 2
 
+class BatchGenerator(Sequence):
+	def __init__(self, messages_by_channel):
+		self._channels = messages_by_channel
+	
+	def __getitem__(self, index):
+		pass
 
-def make_training_data(messages, char_conv):
-	short_messages = (m for m in messages if len(m['content']) < MAX_MSG_LENGTH) #dont want massive messages to limit dimensionality 
-	messages_by_channel = group_messages_by_channel(short_messages)
-    get_timestamp = lambda msg: float(msg['created_at'])
-	messages_by_channel = (sorted(chan, key = get_timestamp) for chan in messages_by_channel) #sort each messages by time if they're not already
+def make_training_data(messages_by_channel, char_conv):
     for channel in messages_by_channel:
 		for index, response in enumerate(channel, 3): #start at msg 3 because I want to the bot to take 3 messages as input to predict the next 
 			msgs = channel[index - 3: index] #last 3 messages before response
-			response_time = get_timestamp(response)
-			response_author = response['author']['id']
 			converted_msgs = []
 			for m in msgs: #create the input
-				content = m['content']
-				int_content = (char_conv.char_to_int[char] for char in content)
-				padding = (0 for _ in range(MAX_MSG_LENGTH - len(content)))
-				time_before_response = normalize_time_delta(response_time - get_timestamp(m))
-				voice = int(m['author']['id'] == response_author) #1 if this is sent by the same person as the response, 0 otherwise
-				converted_msgs = itertools.chain(converted_msgs, int_content, padding, [time_before_response], [voice])
+				time_before_response = normalize_time_delta(response.timestamp - m.timestamp)
+				voice = int(m.author == response.author) #1 if this is sent by the same person as the response, 0 otherwise
+				converted_msgs = itertools.chain(converted_msgs, m.content, padding, [time_before_response], [voice])
 
 
 def create_model(num_inputs, num_outputs, neurons_per_hidden_layer):
@@ -95,13 +117,16 @@ def create_model(num_inputs, num_outputs, neurons_per_hidden_layer):
 	model.add(Dropout(0.2)) #try to not memorize the data
 	model.add(LSTM(neurons_per_hidden_layer))
 	model.add(Dropout(0.2)) 
-	model.add(Dense(num_outputs, activation='softmax')) #output one hot encoded probability for a character
+	model.add(Dense(num_outputs, activation='softmax')) #output probability for each char
 	model.compile(loss='categorical_crossentropy', optimizer='adam') #crossentropy because softmax
+	return model
 
 
 
 if __name__ == "__main__":
 	messages = get_messages()
 	chars = CharConverter(messages)
+	messages_by_channel = preprocess_messages(messages, chars)
+	batch_generator = BatchGenerator(messages_by_channel)
 	model = create_model()
 	model.fit_generator() #use a generator because I have way too much data to stuff into an array
