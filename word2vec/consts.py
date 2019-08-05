5789801bbdb244f34ea4e36cdd53078dfbc9a1e3
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2019-08-05 19:41:02 -0400

    changed some paths to absolute

--- word2vec/consts.py
+++ word2vec/consts.py
@@ -1,6 +1,15 @@
-HOT_ENCODINGS_FILE = "word_one_hot_encoder.json"
+from os import path
 
-WORD2VEC_LUT_FILE = "word2vec.zlib"
+
+WORD2VEC_PATH = path.dirname(__file__)
+
+def abspath(path_end):
+    return path.join(WORD2VEC_PATH, path_end)
+
+
+HOT_ENCODINGS_FILE = abspath("word_one_hot_encoder.json")
+
+WORD2VEC_LUT_FILE = abspath("word2vec.zlib")
 
 NUM_INDEXED_WORDS = 15000
 
