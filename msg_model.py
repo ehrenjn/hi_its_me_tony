3467d35a1e72be9abe8d6ce99003daeacbe2e43f
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2019-08-18 19:24:37 -0400

    refactored TimeSeriesMessage into TimeSeriesInput

--- msg_model.py
+++ msg_model.py
@@ -1,33 +1,3 @@
-#SO WHEN I MAKE THE DISCORD API FOR THIS IT WOULD BE NICE IF COULD SUBCLASS BRAIN INSTEAD OF MAKING A WHOLE NEW CLASS TO JUST HAVE A SINGLE PREDICT FUNCTION AND HAVE DISCORD MESSAGES AUTOMATICALLY CONVERTED TO MessageS
-    #but whatever I don't need it 100%
-#Brain.predict_message is almost certainly fricked because it assumes nothing is wrapped in extra layers of arrays
-#HAVING TO CHECK IF response is None IN TimeSeriesMessage.make_input IS KIND OF A MESS AND HAVING response DEFAULT TO None IS KIND OF BAD TOO
-    #would it be possible to have a TimeSeriesInput that another class in ai.py could inherit from?
-        #this base class wouldn't have a concept of a response; the make_input function would take a response_time parameter
-            #BUT THEN RESPONSE TIME WOULD EITHER HAVE A None DEFAULT OR Brain AND callbacks.py WOULD HAVE TO PROVIDE THE CURRENT time.time()
-                #whatever, that's fine
-
-#~~~WHERE THE FRICK DO I CALL clean_msg?
-    #~~~Brain.predict_message MAKES THE MOST SENSE BUT IT HAS TO TAKE WHOLE MessageS AS INPUT BECAUSE IT NEEDS TO WORK WITH DISCORD AND THE CALLBACK
-    #~~~HMMM RIGHT NOW IM KINDA FEELING THAT THE BEST SOLUTION MIGHT BE FOR THE CALLBACK AND TONY TO DO IT THEMSELVES BUT ITS A BIT GARBAGE BECAUSE ITS JUST A RANDOM API CALL FOR SEEMINGLY NO REASON
-        #~~~THE REASON IT FEELS LIKE IT MAKES SENSE IS BECAUSE THE API IM MAKING HERE OPERATES ON MessageS AND ITS UP TO THE USERS TO FIGURE OUT HOW TO MAKE THEM
-        #~~~BUT HOLD ON: EVERYTHING THAT USES THIS API IS GONNA HAVE A content (except for when ai.py uses Message)... 
-            #~~~SO ACTUALLY NOW THE BEST BET SEEMS TO BE TO HAVE A WAY TO CREATE A Message FROM EITHER words OR content
-                #~~~pretty clean because its the python equivilant of having 2 constructors which is basically what I want
-                #~~~STILL HAVE TO CREATE MessageS MYSELF... but now I don't have to call some weird outside function first
-#~~~GET RID OF Brain.change_model PROBABLY (DONT THINK THE CALLBACK WILL ACTUALLY USE IT AFTERALL)
-#~~~HOW AM I GONNA KEEP TRACK OF CHANNELS AGAIN? honestly I think I only need to do that in the batch generator so I dont need any channel bloat in TimeSeriesMessage
-    #~~~oh yeah and I think this happens before I would even convert to Messages so... epic
-#~~~kind of nasty that Message requires an author_is_tony when we shouldnt really know that until the messages are time series
-    #~~~what if we just passed an author to Message and let TimeSeriesMessage handle it?
-        #~~~WOULDNT WORK when response param of TimeSeriesMessage is None
-    #~~~frick it, I'll just keep it as is right now because it aint super nasty
-#~~~STILL DUNNO IF HAVING TO GIVE A VECTORIZER TO EVERY MESSAGE ACTUALLY MAKES SENSE, IT'S STARTING TO SEEM WEIRD 
-    #~~~but this stuff should be fairly easy to patch later, just make the word_vects function take a vectorizer parameter I guess
-        #~~~or just do that soft vectorizing in TimeSeriesMessage itself because honestly it makes the most sense there
-    #~~~IT LOOKS LIKE TO MAKE A BRAIN AND MAKE MESSAGES ID ALWAYS NEED 2 VECTORIZERS FLOATING AROUND WHICH IS PRETTY NASTY SO PROBABLY SHOULD TAKE IT OUT OF Message
-        #~~~just wait until you finish the generator in ai.py I guess
-
 from keras.models import load_model
 import numpy
 import time
@@ -65,15 +35,13 @@ def normalize_time_delta(delta):
     ) / 2
 
 
-class TimeSeriesMessage:
+class TimeSeriesInput:
+    '''represents a multi-message input to a predictive model'''
     
-    def __init__(self, vectorizer, input_messages, response = None):
+    def __init__(self, vectorizer, input_messages):
         self._vectorizer = vectorizer
         self._input_messages = input_messages
-        self._output_message = response
         self._remove_non_vectorizable_words(vectorizer, *self._input_messages)
-        if self._output_message is not None:
-            self._remove_non_vectorizable_words(vectorizer, self._output_message)
 
 
     def make_input(self, response_time = None):
@@ -102,21 +70,11 @@ class TimeSeriesMessage:
 
         return input_matrix
 
-    
-    def make_output(self):
-        output = numpy.empty((MAX_MSG_LENGTH, WORD2VEC_VEC_SIZE))
-        null_vector = self._vectorizer.to_vect(NULL_WORD)
-        for index in range(len(output)):
-            if index < len(self._output_message):
-                vect = self._vectorizer.to_vect(self._output_message[index])
-                output[index] = vect
-            else:
-                output[index] = null_vector
-        return output
 
     def _remove_non_vectorizable_words(self, vectorizer, *messages):
         for msg in messages:
             msg.words = list(filter(vectorizer.to_vect, msg.words))
+
     
     def __len__(self):
         return sum(
